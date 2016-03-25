ee80afcf8a9a6cba9882dabc221b7ce5e53d7aba
Your Name <you@example.com>
2016-03-25 20:59:27 +0200

    Removed picasso

--- app/src/main/java/com/nethergrim/wallpapers/util/ConnectionUtils.java
+++ app/src/main/java/com/nethergrim/wallpapers/util/ConnectionUtils.java
@@ -1,8 +1,16 @@
 package com.nethergrim.wallpapers.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+
 import com.github.pwittchen.reactivenetwork.library.ConnectivityStatus;
 import com.github.pwittchen.reactivenetwork.library.ReactiveNetwork;
 import com.nethergrim.wallpapers.App;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -20,5 +28,15 @@ public class ConnectionUtils {
                 .observeOn(AndroidSchedulers.mainThread())
                 .map(st -> st.equals(ConnectivityStatus.MOBILE_CONNECTED)
                         || st.equals(ConnectivityStatus.WIFI_CONNECTED));
+    }
+
+    @NonNull
+    public static Bitmap getBitmapFromURL(String src) throws Exception {
+        URL url = new URL(src);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.connect();
+        InputStream input = connection.getInputStream();
+        return BitmapFactory.decodeStream(input);
     }
 }
